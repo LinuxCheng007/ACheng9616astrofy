5937b28e10f26689462d808e98eba8fed7c15660
LinuxCheng007 <ACheng9616@gmail.com>
2024-03-18 17:51:57 +0800

    s da

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+		themes: [
+		  {
+			mytheme: {
+			
+   "primary": "#2f7ddb",
+			
+   "secondary": "#2f7ddb",
+			
+   "accent": "#2f7ddb",
+			
+   "neutral": "#2f7ddb",
+			
+   "base-100": "#ffffff",
+			
+   "info": "#2f7ddb",
+			
+   "success": "#2f7ddb",
+			
+   "warning": "#2f7ddb",
+			
+   "error": "#2f7ddb",
+			},
+		  },
+		],
+	  },
 }
